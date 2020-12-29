e1a0c50aeacae0b1e31aad5d64b6ee28afd095fa
Engin Gökmen <engin.gokmen@clevel.team>
2020-12-29 18:05:31 +0300

    dr

--- app/server.js
+++ app/server.js
@@ -46,3 +46,147 @@ const port = 8000;
 app.listen(port, function() {
   console.log("App now running on port", port);
 });
+
+const jobDetails =
+[
+  {
+  "version":"1.0", "statusCode":200, 
+  "result":{
+    "jobId":"1", "positionName":"Jandarma Bölge Komutanı", "companyName":"Vosyoyvsyvosyv", "durationDay":0,
+    "durationDayText":"Taze çıktı!", "imageUrl":"https://localhost/bluecollar/company/job/Default/6.jpg", "countryCode":"TR",
+    "countryName":"Turkey",
+    "cityName":"İstanbul",
+    "townName":"Ümraniye",
+    "address":"Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey", "postalCode":34768,
+    "hasLatitude":true,
+    "hasLongitude":true,
+    "latitude":41.0266502,
+    "longitude":29.1157803,
+    "description":"syovsvyosvyoyvsoby",
+    "status":1,
+    "contactPhone":{
+      "countryCallingCode":"+90", "areaCode":"879", "number":"9797979"
+     } 
+  }
+},
+{
+  "version":"1.0", "statusCode":200, 
+  "result":{
+    "jobId":"2", "positionName":"Pozisyon 2", "companyName":"Sirket 2", "durationDay":0,
+    "durationDayText":"Taze çıktı!", "imageUrl":"https://localhost/bluecollar/company/job/Default/6.jpg", "countryCode":"TR",
+    "countryName":"Turkey",
+    "cityName":"İstanbul",
+    "townName":"Ümraniye",
+    "address":"Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey", "postalCode":34768,
+    "hasLatitude":true,
+    "hasLongitude":true,
+    "latitude":41.0266502,
+    "longitude":29.1157803,
+    "description":"syovsvyosvyoyvsoby",
+    "status":1,
+    "contactPhone":{
+      "countryCallingCode":"+90", "areaCode":"879", "number":"9797979"
+     } 
+  }
+},
+{
+  "version":"1.0", "statusCode":200, 
+  "result":{
+    "jobId":"3", "positionName":"Pozisyon 3", "companyName":"Sirket 3", "durationDay":0,
+    "durationDayText":"Taze çıktı!", "imageUrl":"https://localhost/bluecollar/company/job/Default/6.jpg", "countryCode":"TR",
+    "countryName":"Turkey",
+    "cityName":"İstanbul",
+    "townName":"Ümraniye",
+    "address":"Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey", "postalCode":34768,
+    "hasLatitude":true,
+    "hasLongitude":true,
+    "latitude":41.0266502,
+    "longitude":29.1157803,
+    "description":"syovsvyosvyoyvsoby",
+    "status":1,
+    "contactPhone":{
+      "countryCallingCode":"+90", "areaCode":"879", "number":"9797979"
+     } 
+  }
+},
+{
+  "version":"1.0", "statusCode":200, 
+  "result":{
+    "jobId":"4", "positionName":"Pozisyon 4", "companyName":"Sirket 4", "durationDay":0,
+    "durationDayText":"Taze çıktı!", "imageUrl":"https://localhost/bluecollar/company/job/Default/6.jpg", "countryCode":"TR",
+    "countryName":"Turkey",
+    "cityName":"İstanbul",
+    "townName":"Ümraniye",
+    "address":"Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey", "postalCode":34768,
+    "hasLatitude":true,
+    "hasLongitude":true,
+    "latitude":41.0266502,
+    "longitude":29.1157803,
+    "description":"syovsvyosvyoyvsoby",
+    "status":1,
+    "contactPhone":{
+      "countryCallingCode":"+90", "areaCode":"879", "number":"9797979"
+     } 
+  }
+},
+{
+  "version":"1.0", "statusCode":200, 
+  "result":{
+    "jobId":"5", "positionName":"Pozisyon 5", "companyName":"Sirket 5", "durationDay":0,
+    "durationDayText":"Taze çıktı!", "imageUrl":"https://localhost/bluecollar/company/job/Default/6.jpg", "countryCode":"TR",
+    "countryName":"Turkey",
+    "cityName":"İstanbul",
+    "townName":"Ümraniye",
+    "address":"Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey", "postalCode":34768,
+    "hasLatitude":true,
+    "hasLongitude":true,
+    "latitude":41.0266502,
+    "longitude":29.1157803,
+    "description":"syovsvyosvyoyvsoby",
+    "status":1,
+    "contactPhone":{
+      "countryCallingCode":"+90", "areaCode":"879", "number":"9797979"
+     } 
+  }
+}
+]
+
+const jobList =
+{
+  "version":"1.0", "statusCode":200, 
+  "result":{
+    "items":[ 
+      {
+      "jobId":"1", "positionName":"Gerber Operatörü", "companyName":"Testoğlu Lahmacun Pide Ve Kebap", "durationDay":0,
+      "durationDayText":"Taze çıktı!",
+      "imageUrl":"http://localhost/bluecollar/Company/Job/000/000/000/avatar/ 13920170713010155805.jpg",
+      "distance":"8,53 km yakınında", "latitude":41.050968170166023, "longitude":29.062856674194339, "cityName":"İstanbul", "townName":"Çengelköy Mahallesi"
+      },
+      {
+      "jobId":"2", "positionName":"Pozisyon 2", "companyName":"Sirket 2", "durationDay":0,
+      "durationDayText":"Taze çıktı!",
+      "imageUrl":"http://localhost/bluecollar/Company/Job/000/000/000/avatar/ 13920170713010155805.jpg",
+      "distance":"8,53 km yakınında", "latitude":41.050968170166023, "longitude":29.062856674194339, "cityName":"İstanbul", "townName":"Çengelköy Mahallesi"
+      },
+      {
+      "jobId":"3", "positionName":"Pozisyon 3", "companyName":"Sirket 3", "durationDay":0,
+      "durationDayText":"Taze çıktı!",
+      "imageUrl":"http://localhost/bluecollar/Company/Job/000/000/000/avatar/ 13920170713010155805.jpg",
+      "distance":"8,53 km yakınında", "latitude":41.050968170166023, "longitude":29.062856674194339, "cityName":"Ağrı", "townName":"Çengelköy Mahallesi"
+      },
+      {
+      "jobId":"4", "positionName":"Pozisyon 4", "companyName":"Sirket 4", "durationDay":0,
+      "durationDayText":"Taze çıktı!",
+      "imageUrl":"http://localhost/bluecollar/Company/Job/000/000/000/avatar/ 13920170713010155805.jpg",
+      "distance":"8,53 km yakınında", "latitude":41.050968170166023, "longitude":29.062856674194339, "cityName":"Ankara", "townName":"Çengelköy Mahallesi"
+      },
+      {
+      "jobId":"5", "positionName":"Pozisyon 5", "companyName":"Sirket 5", "durationDay":0,
+      "durationDayText":"Taze çıktı!",
+      "imageUrl":"http://localhost/bluecollar/Company/Job/000/000/000/avatar/ 13920170713010155805.jpg",
+      "distance":"8,53 km yakınında", "latitude":41.050968170166023, "longitude":29.062856674194339, "cityName":"İzmir", "townName":"Çengelköy Mahallesi"
+      } 
+    ],
+    "total":5 
+  }
+}
